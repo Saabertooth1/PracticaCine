6c1d007bf838d40f5d92ac1c1f5a0516f53385ff
Saabertooth1 <nalas3@icloud.com>
2017-06-22 12:53:05 +0200

    Correcciones Pablo aplicadas.

--- src/cine/Sala.java
+++ src/cine/Sala.java
@@ -29,7 +29,7 @@ public class Sala {
         for (int i = 0; i < horasSesiones.length; i++){
             int pos = 0;
             Sesion sesion = new Sesion(horasSesiones[i], filas, columnas);
-            for (int j = 0; j < sesiones.size(); j++){
+            for (int j = 0; sesiones.size() > j && j < sesiones.size(); j++){
                 if (sesiones.get(j).getHora().compareTo(horasSesiones[i]) < 0){
                     pos++;
                 }
@@ -42,13 +42,18 @@ public class Sala {
     }//De constructor
 
     public void comprarEntrada (int sesion, int fila, int columna){ //Método para comprar entradas en una sesión dada
-        sesiones.get(sesion).comprarEntrada(fila, columna);
-
+        if(sesiones.size() > sesion - 1){
+            sesiones.get(sesion- 1).comprarEntrada(fila, columna);
+        }
     }//De comprarEntrada
 
     public int getIdEntrada (int sesion, int fila, int columna){ //Devuelve el id de venta asociado a una sesión, fila y  columna
-        return sesiones.get(sesion).getIdEntrada(fila, columna);
-
+        if(sesiones.size() > sesion - 1){
+            return sesiones.get(sesion-1).getIdEntrada(fila, columna);
+        }
+        else{
+            return 0;
+        }
     }//De getIdEntrada
 
     public String[] getHorasDeSesionesDeSala (){ //Método que devuelve las horas de sesiones asociadas a una sala
@@ -64,7 +69,7 @@ public class Sala {
     }//De getHorasDeSesionesDeSala
 
     public char[][] getEstadoSesion (int sesion){ //Devuelve la matriz asociada al estado de una sesión donde un asiento vacío está representado por "O" y un asiento ocupado por "#"
-        return sesiones.get(sesion).getEstadoSesion();
+        return sesiones.get(sesion-1).getEstadoSesion();
 
     }//De getEstadoSesion
 
@@ -74,28 +79,28 @@ public class Sala {
     }//De getPelicula
 
     public String recogerEntradas (int id, int sesion){ //Dado un id de venta y una sesión, devuelve un String con la película, la hora de la sesión y los asientos asociados a ese id
-        if (this.sesiones.get(sesion).recogerEntradas(id) == null){
+        if (this.sesiones.get(sesion-1).recogerEntradas(id) == null){
             return null;
 
         }
         else{
-            return this.pelicula + "@" + this.sesiones.get(sesion).recogerEntradas(id);
+            return this.pelicula + "@" + this.sesiones.get(sesion-1).recogerEntradas(id);
         }
 
     }//De recogerEntradas
 
     public int getButacasDisponiblesSesion (int sesion){ //Devuelve el número de butacas disponibles para una sesión
-        return sesiones.get(sesion).getButacasDisponiblesSesion();
+        return sesiones.get(sesion-1).getButacasDisponiblesSesion();
 
     }//De getButacasDisponiblesSesion
 
     public ButacasContiguas recomendarButacasContiguas (int noButacas, int sesion){ //Método recomendar butacas para una sesión dada
-        return sesiones.get(sesion).recomendarButacasContiguas(noButacas);
+        return sesiones.get(sesion-1).recomendarButacasContiguas(noButacas);
 
     }//De recomendarButacasContiguas
 
     public void comprarEntradasRecomendadas (int sesion, ButacasContiguas butacas){ //Compra las entradas recomendadas por el método recomendarButacasContiguas
-        sesiones.get(sesion).comprarEntradasRecomendadas(butacas);
+        sesiones.get(sesion-1).comprarEntradasRecomendadas(butacas);
 
     }//De comprarEntradasRecomendadas
 
@@ -103,7 +108,10 @@ public class Sala {
         int pos = 0;
         Sesion sesion = new Sesion(horaSesion, filas, columnas);
         for (int i = 0; i < sesiones.size(); i++){
-            if (sesiones.get(i).getHora().compareTo(horaSesion) < 0){
+            if (sesiones.size() == 0 || sesiones.get(i).getHora().compareTo(horaSesion) >= 0){
+                i = sesiones.size();
+            }
+            else{
                 pos++;
             }
 
@@ -114,7 +122,10 @@ public class Sala {
     public void borrarSesion (String horaSesion){ //Elimina una sesión de una sala
         int i = 0;
         while (i < sesiones.size()){
-            if (sesiones.get(i).getHora().equals(horaSesion)){
+            if(sesiones.size() == 0){
+                i = sesiones.size() + 1;
+            }
+            else if (sesiones.get(i).getHora().equals(horaSesion)){
                 sesiones.removeElementAt(i);
             }
             i++;
